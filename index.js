2ff0ad3cab63bd73cab03989b7aa7a1788fdae4d
mamurjon1148 <mamurjonovmamurjon01@gmail.com>
2024-06-14 19:18:14 +0500

    done

---
+++ index.js
@@ -0,0 +1,76 @@
+// 1 - vazifa
+// let son1 = 10;
+// let son2 = 5;
+
+// console.log(son1 + son2);
+
+
+
+
+// 2-vazifa
+
+// let a = "12";
+// let b = Number(a);
+
+// console.log(b);
+
+
+
+
+
+// 3-vazifa
+
+// let randomson = Math.floor(Math.random() * 100);
+
+// if (randomson % 2 === 0) {
+//     console.log(`${randomson} -> juft son`)
+// } else {
+//     console.log(`${randomson} -> toq son`)
+// }
+
+
+
+
+// 4-vazifa
+
+// let soz = String(prompt("So'zni kiriting"));
+
+// if (soz === "Mars") {
+//     alert("Mars")
+// } else {
+//     alert("Mars it school")
+// }
+
+
+
+
+// 5-vazifa
+
+// let emblemalar = ["Lamborghini", "BMW", "Audi", "Rolce Roys", "Mercedes-Benz"]
+// let beshdankatta = []
+// let beshdankichik = []
+
+// for (let emblema of emblemalar) {
+//     if (emblema.length > 5) {
+//         beshdankatta.push(emblema)
+//     } else {
+//         beshdankichik.push(emblema)
+//     }
+// }
+
+// console.log(beshdankatta);
+// console.log(beshdankichik);
+
+
+
+
+// 6-vazifa
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} -> juft son`)
+    } else {
+        console.log(`${i} -> toq son`)
+    }
+}
+
